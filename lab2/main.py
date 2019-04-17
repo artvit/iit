57c6463d7ba26ba4c52c2f38d26a929420afad9e
artvit <artvitk@gmail.com>
2019-04-17 14:24:27 +0300

    Add search

--- lab2/main.py
+++ lab2/main.py
@@ -1,12 +1,13 @@
 from searchsystem import SearchSystem
 
+
 def main():
     ss = SearchSystem('docs')
 
     for w in ['science', 'language', 'scientist', 'knowledge']:
-        relevance = ss.search(w)
+        results = ss.search(w)
         print(f'"{w}" search results:')
-        print(*relevance, sep='\n')
+        print(*results, sep='\n')
         print()
 
 
--- lab2/searchsystem.py
+++ lab2/searchsystem.py
@@ -12,7 +12,7 @@ annotations_label = 'annotation'
 words_label = 'words'
 
 
-split_words = '[^a-zA-Z]'
+split_words_regexp = '[^a-zA-Z]'
 
 
 def get_files_list(path):
@@ -24,21 +24,30 @@ def get_files_list(path):
     return files
 
 
-def get_words(node, parent=None):
+def get_words(node):
     words = []
-    annotations = []
     if not node.tag:
-        t_words = re.split(split_words, node.content)
-        if parent == 'bt':
-            annotations = t_words
-        else:
-            words = t_words
+        words = re.split(split_words_regexp, node.content)
     elif node.children:
         for child in node.children:
-            t_words, t_annotations = get_words(child, node.tag)
-            words.extend(t_words)
-            annotations.extend(t_annotations)
-    return words, annotations
+            words.extend(get_words(child))
+    return words
+
+
+def cosine_similarity(query_idf_dict, idf_dict):
+    dot_product = 0
+    for word in query_idf_dict:
+        dot_product += query_idf_dict[word] * idf_dict[word]
+
+    delimiter = l2_norm(query_idf_dict) * l2_norm(idf_dict)
+    return dot_product / delimiter
+
+
+def l2_norm(idf_dict):
+    sum = 0
+    for w in idf_dict:
+        sum += idf_dict[w] ** 2
+    return math.sqrt(sum)
 
 
 class SearchSystem:
@@ -70,74 +79,43 @@ class SearchSystem:
 
     def parse_docs(self):
         for f, tree in self.files.items():
-            words, annotations = get_words(tree)
-            self.docs[f] = {
-                annotations_label: {
-                    'coef': annotation_coef,
-                    'words': list(filter(None, annotations))
-                },
-                words_label: {
-                    'coef': general_text_coef,
-                    'words': list(filter(None, words))
-                }
-            }
+            words = get_words(tree)
+            self.docs[f] = list(filter(None, words))
 
     def create_all_words_set(self):
         for name, words in self.docs.items():
-            annotations = set(words[annotations_label]['words'])
-            main_text = set(words[words_label]['words'])
-            for word in list(annotations) + list(main_text):
-                self.all_words.add(word)
-
-    def calc_doc_len(self):
-        avg_len = 0
-
-        for name, doc in self.docs.items():
-            annotations = doc[annotations_label]['words']
-            main_text = doc[words_label]['words']
-
-            doc_len = len(annotations) + len(main_text)
-            self.doc_len[name] = doc_len
-            avg_len += doc_len
-
-        self.avg_len = avg_len / len(self.docs)
+            self.all_words.update(words)
 
     def create_tf_index(self):
         self.init_dict(self.tf)
+        avg_len = 0
         for file, words in self.docs.items():
-            annotations_part = words[annotations_label]
-            annotations = annotations_part['words']
-            annotations_coef = annotations_part['coef']
-            annotations_set = set(annotations)
+            words_set = set(words)
 
-            main_text_part = words[words_label]
-            main_text = main_text_part['words']
-            main_text_coef = main_text_part['coef']
-            main_text_set = set(main_text)
+            doc_len = len(words)
+            self.doc_len[file] = doc_len
+            avg_len += doc_len
 
-            doc_len = len(annotations) + len(main_text)
-
-            for term in list(annotations_set) + list(main_text_set):
+            for term in words:
                 self.tf[file][term] = 0
 
-            for term in annotations_set:
-                self.tf[file][term] += annotations_coef * annotations.count(term) / doc_len
+            for term in words_set:
+                self.tf[file][term] += words.count(term) / doc_len
 
-            for term in main_text_set:
-                self.tf[file][term] += main_text_coef * main_text.count(term) / doc_len
+        self.avg_len = float(avg_len) / len(self.docs)
 
     def create_idf_index(self):
-        self.init_dict(self.idf)
+        self.idf = {}
         len_docs = len(self.tf)
 
         for word in self.all_words:
             occurrence_in_docs = 0
 
-            for tf in self.tf.values():
-                if tf[word]:
-                    occurrence_in_docs += 1
+            for words in self.docs.values():
+                if word in words:
+                    occurrence_in_docs += 1.0
 
-            self.idf[word] = math.log(len_docs / occurrence_in_docs) if occurrence_in_docs else 0
+            self.idf[word] = math.log(len_docs / occurrence_in_docs) + 1.0 if occurrence_in_docs else 0
 
     def create_tf_idf_index(self):
         self.init_dict(self.tf_idf)
@@ -146,10 +124,67 @@ class SearchSystem:
                 self.tf_idf[file][word] = self.tf[file][word] * self.idf[word]
 
     def init_dict(self, d):
-        for name in self.docs:
-            d[name] = {}
-
+        for file in self.docs:
+            d[file] = {}
             for word in self.all_words:
-                d[name][word] = 0
+                d[file][word] = 0
+
+    def search(self, query):
+        relevance = dict()
+
+        if query and query.strip():
+            query_tf = self.calc_query_tf(query)
+            query_tf_idf = self.calc_query_tf_idf(query_tf)
+            relevance = self.calc_doc_relevance(query_tf_idf)
+
+        return relevance
+
+    def calc_query_tf(self, query):
+        query_tf = {}
+
+        query = query.lower().strip()
+        terms = re.split(split_words_regexp, query)
+
+        terms_len = len(terms)
+        terms_set = set(terms)
+
+        if not terms_len:
+            return
+
+        for term in terms_set:
+            query_tf[term] = terms.count(term) / terms_len
+
+        return query_tf
+
+    def calc_query_tf_idf(self, query_tf):
+        query_tf_idf = {}
+
+        for word, tf in query_tf.items():
+            if word in self.idf:
+                query_tf_idf[word] = tf * self.idf[word]
+            else:
+                query_tf_idf[word] = 0
+
+        return query_tf_idf
+
+    def calc_doc_relevance(self, query_tf_idf):
+        docs_rating = {}
+        k1 = 2
+        b = 0.75
+
+        # for file, doc in self.docs.items():
+        #     doc_score = 0
+        #     for term in query_tf_idf:
+        #         if term in self.idf:
+        #             div = self.idf[term] * self.tf[file][term] * (k1 + 1)
+        #             delim = (self.tf[file][term] + k1 * (1 - b + b * self.doc_len[file] / self.avg_len))
+        #             doc_score += div / delim
+        #
+        #     docs_rating[file] = doc_score
+
+        for file, tfidf_dict in self.tf_idf.items():
+            docs_rating[file] = cosine_similarity(query_tf_idf, tfidf_dict)
+
+        return sorted(docs_rating.items(), key=lambda a: a[1], reverse=True)
 
 
